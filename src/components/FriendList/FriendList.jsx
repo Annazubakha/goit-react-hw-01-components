2f67617f7cd1511c1ac004861d1d72eb813ad85d
Annazubakha <korepovaanna@gmail.com>
2023-12-28 17:18:23 +0100

    added tasks without styles

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,10 @@
+import { FriendListItem } from 'components/FriendListItem/FriendListItem';
+export const FriendList = ({ friends }) => {
+  return (
+    <ul className="friend-list">
+      {friends.map(friend => (
+        <FriendListItem key={friend.id} {...friend} />
+      ))}
+    </ul>
+  );
+};
